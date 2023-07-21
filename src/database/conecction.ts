3e0e1d83f3986c6995479b3990e9f18717e21dee
Walter_Gomez <129338273+WalterFernandoGomezSantamaria@users.noreply.github.com>
2023-07-21 16:03:34 -0500

    update mongo connection

--- src/database/conecction.ts
+++ src/database/conecction.ts
@@ -5,8 +5,7 @@ dotenv.config();
 
 export const connection = async (): Promise<void> => {
     try {
-        await mongoose.connect(
-            `mongodb://${process.env.USER}:${process.env.PASS}@localhost:${process.env.PORT}/${process.env.DTANAME}?authSource=admin`);
+        await mongoose.connect(`${process.env.DB_HOST}`)
         console.log('Conexión exitosa a la base de datos');
     } catch (error) {
         throw new Error(`No se ha podido conectar con la base de datos: ${error}`);
